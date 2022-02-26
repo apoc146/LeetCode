26338a1448cae96a522cb7a934e1bb7208c35b17
Shivam <shivambhat02@gmail.com>
2022-02-27 03:09:27 +0530

    Time: 211 ms (35.56%), Space: 93.3 MB (90.36%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    int maxProfit(vector<int>& prices) {
+        /**
+         * @brief l,r = left and right day
+         * if +ve gradient, then r++ since we can still find greater peak
+         * if -ve gradient , l++ and r++ since we dont want this
+         * while doing this keep a track of largest +ve gradient
+         * 
+         */
+
+        int maxDiff=0; 
+        int l=0,r=0;
+        while(r<prices.size()){
+            //found +ve gradient
+            if(prices[r]>=prices[l]){
+                maxDiff=max(maxDiff,prices[r]-prices[l]);
+                r++;
+            }else{
+                //found -ve gradient, so skip this
+                l=r;
+                r++;
+            }
+        }
+        return maxDiff;
+    }
+};
