4bbee7143c8daae3d89d60e33b153158bc72d846
Shivam <shivambhat02@gmail.com>
2022-03-08 22:04:00 +0530

    Time: 4 ms (24.04%), Space: 5.9 MB (23.50%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,19 +5,29 @@ class Solution {
 public:
     
     int binary(int a,int b,int n){
+        
         int mid=a+(b-a)/2;
         if(isBadVersion(mid)==true){
+            
+            //1st element
             if(mid==1){
                 return 1;
             }
+            
+            //last element
             if(mid==n && isBadVersion(n-1)==false){
                  return n;
             }
+            
+            //mid element 
             if(isBadVersion(mid-1)==false && isBadVersion(mid+1)==true){
                 return mid;
             }
+            
+            // for FTT(mid)TT
             return binary(a,mid-1,n);
         }
+        
         return binary(mid+1,b,n);
     }
     
