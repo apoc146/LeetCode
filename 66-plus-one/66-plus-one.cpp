e2ee7a3d798c5ab7aeb12286cb33d046251f724a
Shivam <shivambhat02@gmail.com>
2022-07-29 03:22:32 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (98.11%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+        // my recursive function 
+    void foo(vector<int>& vec, int carry, int lsb){
+        if(carry==0){
+            //do nothing
+            return ;
+        }
+        
+        if(lsb>=vec.size()){
+            vec.insert(vec.begin(),1);
+            return ;
+        }
+        
+        int vecSize=vec.size();
+        int num=vec[vecSize-1-lsb];
+        if(num<9){
+            vec[vecSize-1-lsb]++;
+            foo(vec,0,lsb+1);
+        }else{
+            vec[vecSize-1-lsb]=0;
+            foo(vec,1,lsb+1);
+        }
+    }
+    
+    vector<int> plusOne(vector<int>& digits) {
+        foo(digits,1,0);
+        return digits;
+    }
+};
