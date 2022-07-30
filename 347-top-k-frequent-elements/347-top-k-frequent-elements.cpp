a5548bf43f955a0b6bb1a3b56816113902be7c31
Shivam <shivambhat02@gmail.com>
2022-07-31 01:56:16 +0530

    Time: 12 ms (97.57%), Space: 13.8 MB (43.28%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-                
-        //(key,val)
+            
         unordered_map<int,int> m1;
         unordered_map<int,int>::iterator it;
-        
-        //m2 is reverse of m1
-        //(val,key)
-        multiset<int,int> m2;
+
+        //make map of key,count
         for(int i=0;i<nums.size();i++){
             m1[nums[i]]++;
         }
         
+        //vector of pair(count,key)
+        //cant use maps since count can be same, maps need unique keys
         vector<pair<int,int>> heap;
+        
         for(it=m1.begin();it!=m1.end();it++){
             heap.push_back(make_pair(it->second,it->first));
         }
         
+        //make heap of above vector
         make_heap(heap.begin(),heap.end());
         
         vector<int> res;
         
+        //pop heap K times
         for(int i=0;i<k;i++){
             res.push_back(heap.front().second);
             pop_heap(heap.begin(),heap.end());
             heap.pop_back();
         }
         
+        //Ans
         return res;
         
     }
