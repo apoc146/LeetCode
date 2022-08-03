a5ebdaffe62b196eff1e9cd54dd290b8034f9b6e
Shivam <shivambhat02@gmail.com>
2022-08-04 00:17:30 +0530

    Time: 1788 ms (5.00%), Space: 229 MB (5.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,21 +3,27 @@ public:
     int lengthOfLongestSubstring(string s) {
         int maxlen=0;
         int i=0;
+        
+        //start at index i
         while(i<s.size()){
             int j=i;
             set<char> set;
             unordered_map<char,int> map;
             
-            
+            //loop until you find unique characters
+            //maps is used to keep track of characters and their indices
             while(j<s.size() && map.count(s[j])==0){
                 map[s[j]]=j;
                 j++;
             }
             
+            //breaks off the loop only if -> duplicate char or reaching end of string
             maxlen=max(maxlen,j-i);
             if(j<s.size()){
+                //detected repeating char so now move 'i' to next of the index of first occurance of this repeated character
                 i=map[s[j]]+1;
             }else{
+                //reached end of string so i++
                 i++;
             }
         }
