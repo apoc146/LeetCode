6700c8f5dabd093d7e87d8c1d726b87fd5ecfb07
Shivam <shivambhat02@gmail.com>
2022-08-03 14:27:17 +0530

    Time: 208 ms (31.37%), Space: 51.5 MB (11.08%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+
+        // checks
+        if(nums.front()>0){
+            return res;
+        }
+        
+        
+//         if(nums.front()==0 && nums.back()==0){
+//             nums.resize(3);
+//             res.push_back(nums);
+//             return res;
+//         };
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(i>0 && nums[i]==nums[i-1])continue;
+            
+            // now do two sum
+            int target= 0-nums[i];
+            
+            vector<int> numsT(nums.begin()+i+1,nums.end());
+            int j=0;
+            int k=numsT.size()-1;
+            while(j<k){
+                int sum=numsT[j]+numsT[k];
+                if(sum==target){
+                    vector<int> resT;
+                    resT.push_back(nums[i]);
+                    resT.push_back(numsT[j]);
+                    resT.push_back(numsT[k]);
+                    res.push_back(resT);
+                    if(numsT[j]==numsT[k]){
+                        break;    
+                    }
+                    j++;
+                    k--;
+                }else if(sum<target){
+                    j++;
+                }else{
+                    k--;
+                }
+            }
+        }
+        
+        sort(res.begin(),res.end());
+        res.erase(unique(res.begin(),res.end()),res.end());
+        //Remove Duplicate by converting to set -> back to vector
+        // set<vector<int>> s(res.begin(),res.end());
+        // res.assign(s.begin(),s.end());
+        return res;
+    }
+};
