35a19baa81ab7bb82e5787a6ed30752b9c16191b
Shivam <shivambhat02@gmail.com>
2022-08-04 00:13:23 +0530

    Time: 1539 ms (5.00%), Space: 228.9 MB (5.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -10,20 +10,16 @@ public:
             
             
             while(j<s.size() && map.count(s[j])==0){
-                // cout<<"test";
                 map[s[j]]=j;
-                // set.insert(s[j]);
                 j++;
             }
             
             maxlen=max(maxlen,j-i);
             if(j<s.size()){
                 i=map[s[j]]+1;
-
             }else{
                 i++;
             }
-            // map[s[j]]=j;
         }
         return maxlen;
     }
