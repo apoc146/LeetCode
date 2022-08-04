2dc1ef28b2367a3e937affe6f660532484e8fd77
Shivam <shivambhat02@gmail.com>
2022-08-04 17:14:22 +0530

    Time: 9 ms (84.89%), Space: 7.1 MB (51.45%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,33 +2,25 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int res=0;
-        int l=0;
+        int l=0;                        //i and j to implement sliding window
         int r=0;
-        unordered_map<char,int> map;
-        int max_freq_char_count=0;
+        unordered_map<char,int> map;    //map to track count of all characters in window
+        int max_freq_char_count=0;      //Highest count from the map above
         
         while(r<s.size()){
+            //increment map count and update frequence count.
             map[s[r]]++;
             max_freq_char_count=max(max_freq_char_count,map[s[r]]);
-            int diff_char_count=r-l+1-max_freq_char_count;
-            
-            bool condition= (diff_char_count <= k);
-            
+
+            //while the condition is not true decrease the window by i++. Accordingly reduce count of those chars.
             while(r-l+1-max_freq_char_count>k){
                 map[s[l]]--;
                 l++;
             }
             
+            //find length of window once condition is satisfied(exists while loop)
             res=max(res,r-l+1);
             r++;
-            
-//             if(condition==true){
-//                 res=max(res,r-l+1);
-//                 r++;
-//             }else{
-//                 map[s[l]]--;
-//                 l++;
-//             }
         }
         return res;
     }
