28a841e47b7724af1559a182a150148e35653996
Shivam <shivambhat02@gmail.com>
2023-08-13 16:56:32 -0400

    Time: 2 ms (39.56%), Space: 6.2 MB (14.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-    // //Recursive solution 
-    //     if(n==1){
-    //         return 1;
-    //     } 
-    //     if(n==2){
-    //         return 2;
-    //     }
-    //     if(n<1){
-    //         return 0;
-    //     }
-    //     return climbStairs(n-1)+climbStairs(n-2);
-    // }
-        
-        if(n<3){
-            return n;
-        }
-        int dp[n+1];
-        memset(dp,0,sizeof(dp));
+    int foo(int n){
+        vector<int> dp(n+2,0);
         dp[0]=0;
         dp[1]=1;
         dp[2]=2;
         
-        for(int i=3;i<=n;i++){
+        for(int i=3;i<n+1;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         
         return dp[n];
+    }
+    
+    
+//     int foo2(int n){
+//         if(n==1){
+//             return 1;
+//         }
+        
+//         if(n==2){
+//             return 2;
+//         }    
+    
+//         return (foo(n-1)+foo(n-2));
+//     }
+    
+    int climbStairs(int n) {
+        int ans=foo(n);
+        return ans;
     }
 };
