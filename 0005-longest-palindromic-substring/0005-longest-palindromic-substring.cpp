84a229cbf13866573e3f720ad15a35f1f8cff06a
Shivam <shivambhat02@gmail.com>
2023-08-19 19:50:10 -0400

    Time: 31 ms (64.51%), Space: 159.5 MB (25.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,45 +1,35 @@
+/*
+    Given a string s, return the longest palindromic substring in s
+    Ex. s = "babad" -> "bab", s = "cbbd" -> "bb"
+
+    Expand around center, extend as far as possible, store max length
+
+    Time: O(n^2)
+    Space: O(1)
+*/
+
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.size();
-        bool isEven=(n%2==0);
-        int maxlen=0;
-        string res="";
-        int l=0;
-        int r=0;
-        for(int i=0;i<n;i++){
-            //ODD
-            l=i;
-            r=i;
-            while((l>=0 && r<=n-1) && (s[l]==s[r])){
-                if((r-l+1)>=maxlen){
-                    maxlen=r-l+1;
-                    res=s.substr(l,r-l+1);
-                    cout<<res<<endl;
-                }
-                
-                l--;
-                r++;       
-            }
-
-            l=i;
-            r=i+1;
-            while((l>=0 && r<=n-1) && (s[l]==s[r])){
-                if((r-l+1)>=maxlen){
-                    maxlen=r-l+1;
-                    res=s.substr(l,r-l+1);   
-                }
-                
-                l--;
-                r++;       
-            }
-
+        int maxStart = 0;
+        int maxLength = 1;
+        
+        for (int i = 0; i < s.size() - 1; i++) {
+            middleOut(s, i, i, maxStart, maxLength);
+            middleOut(s, i, i + 1, maxStart, maxLength);
         }
         
-        cout<<" l:"<<l<<" r:"<<r<<endl;
-        if(res==""){
-            res=s[0];
+        return s.substr(maxStart, maxLength);
+    }
+private:
+    void middleOut(string s, int i, int j, int& maxStart, int& maxLength) {
+        while (i >= 0 && j <= s.size() - 1 && s[i] == s[j]) {
+            i--;
+            j++;
         }
-        return res;
+        if (j - i - 1 > maxLength) {
+            maxStart = i + 1;
+            maxLength = j - i - 1;
+        }
     }
 };
