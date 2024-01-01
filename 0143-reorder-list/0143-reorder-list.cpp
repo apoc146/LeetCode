65c4d1c3845266e7959856a14d08ddbf84e110d7
Shivam <shivambhat02@gmail.com>
2024-01-01 17:48:54 -0500

    Time: 23 ms (84.66%), Space: 18.1 MB (73.27%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverList(ListNode* head){
+        if(head==NULL)return head;
+        
+        ListNode* temp=NULL;
+        ListNode* prev=NULL;
+        ListNode* cur=head;
+        
+        while(cur!=NULL){
+            temp=prev;
+            prev=cur;
+            cur=cur->next;
+            prev->next=temp;
+        }
+        
+        return prev;
+    }
+    
+    void merge(ListNode* l1, ListNode* l2){
+        while (l2) {
+          ListNode* next = l1->next;
+          l1->next = l2;
+          l1 = l2;
+          l2 = next;
+        }
+    }
+    
+    void reorderList(ListNode* head) {
+    
+        if(head->next==NULL)return ;
+        //find middle element;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* prev=NULL;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        prev->next=NULL;
+        
+        ListNode* returnHead;
+        ListNode* l1=head;
+        ListNode* l2=reverList(slow);
+        ListNode* t1=l1;
+        ListNode* t2=l2;
+        returnHead=head;
+        
+        // Merging two lists l1 and l2;
+        merge(l1,l2);
+//         while(l1!=NULL){
+//             t1=l1->next;
+//             l1->next=l2;
+//             l1=t1;
+            
+//             if(l1==NULL){
+//                 break;
+//             }
+            
+//             t2=l2->next;
+//             l2->next=l1;
+//             l2=t2;
+//         }
+    }
+};
