efd71a85bafdcd2d5c3b6b380fcbd5fe721cb26d
Shivam <shivambhat02@gmail.com>
2024-01-01 17:51:20 -0500

    Time: 59 ms (11.73%), Space: 18.1 MB (73.27%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    ListNode* reverList(ListNode* head){
+    ListNode* reverseList(ListNode* head){
         if(head==NULL)return head;
         
         ListNode* temp=NULL;
@@ -48,31 +48,13 @@ public:
             prev=slow;
             slow=slow->next;
             fast=fast->next->next;
-        }
-        
+        } 
         prev->next=NULL;
         
-        ListNode* returnHead;
         ListNode* l1=head;
-        ListNode* l2=reverList(slow);
-        ListNode* t1=l1;
-        ListNode* t2=l2;
-        returnHead=head;
-        
+        ListNode* l2=reverseList(slow);
+
         // Merging two lists l1 and l2;
         merge(l1,l2);
-//         while(l1!=NULL){
-//             t1=l1->next;
-//             l1->next=l2;
-//             l1=t1;
-            
-//             if(l1==NULL){
-//                 break;
-//             }
-            
-//             t2=l2->next;
-//             l2->next=l1;
-//             l2=t2;
-//         }
     }
 };
