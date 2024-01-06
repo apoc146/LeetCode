be7ccf05c1ea24fd99ccb7493034e549627c8238
Shivam <shivambhat02@gmail.com>
2024-01-06 15:42:10 -0500

    LC 101

---
+++ 101.symmetric-tree.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=101 lang=cpp
+ *
+ * [101] Symmetric Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool foo(TreeNode* r1, TreeNode* r2){
+        if(!r1 and !r2){
+            return true;
+        }
+        if(!r1 and r2 or r1 and !r2){
+            return false;
+        }
+        return (r1 and r2 and (r1->val == r2->val) and foo(r1->left,r2->right) and foo(r1->right,r2->left));
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root){
+            return true;
+        }
+        return foo(root->left, root->right);
+    }
+};
+// @lc code=end
+
