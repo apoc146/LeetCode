3d0e184ef1594c19132a23c852810f4ca5168e02
Shivam <shivam97rules@gmail.com>
2024-01-12 20:44:56 -0500

    Time: 50 ms (20.49%), Space: 18.8 MB (25.06%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public: 
+    bool check(int i,int j, int row, int col){
+        if(i<0 or i>=row or j<0 or j>=col){
+            return false;
+        }
+        return true;
+    }
+
+void dfs(int i, int j, vector<vector<char>>& graph, vector<vector<int>>& visited) {
+        int r = graph.size();
+        int col = graph[0].size();
+
+        if (!check(i, j, r, col) || graph[i][j] == '0') {
+            return;
+        }
+        visited[i][j] = 1;
+        vector<pair<int, int>> neighs({{0, -1}, {-1, 0}, {0, +1}, {+1, 0}});
+        for (int c = 0; c < 4; c++) {
+            int xx = i + neighs[c].first;
+            int yy = j + neighs[c].second;
+            if (check(xx, yy, r, col) && graph[xx][yy] == '1' && !visited[xx][yy]) {
+                dfs(xx, yy, graph, visited);
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int r = grid.size();
+        int col = grid[0].size();
+        int count = 0;
+
+        vector<vector<int>> visited(r, vector<int>(col, 0));
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < col; j++) {
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    count++;
+                    dfs(i, j, grid, visited);
+                }
+            }
+        }
+        return count;
+    }
+};
