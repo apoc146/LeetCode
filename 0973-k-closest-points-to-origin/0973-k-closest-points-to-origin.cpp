a5a20aec24480a12523cbf60bb3f65cba836537a
Shivam <shivam97rules@gmail.com>
2024-02-08 18:33:37 -0500

    Time: 604 ms (11.95%), Space: 184.1 MB (6.25%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,18 +1,21 @@
-class comp {
-public:
-    bool operator()(const std::vector<int>& a, const std::vector<int>& b) {
-        int dist1 = pow(a[0], 2) + pow(a[1], 2);
-        int dist2 = pow(b[0], 2) + pow(b[1], 2);
-        return dist1 > dist2;
+class comp{
+  public:
+    bool operator()(vector<int> a,vector<int> b){
+        int dist1=pow(a[0],2) + pow(a[1],2);
+        int dist2=pow(b[0],2) +  pow(b[1],2);
+        if(dist1<dist2){
+            return false;
+        }
+        return true;
     }
 };
 
 class Solution {
 public:
-    std::vector<std::vector<int>> kClosest(std::vector<std::vector<int>>& points, int k) {
-        std::vector<std::vector<int>> ans;
-        std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, comp> heap(points.begin(), points.end());
-        while (k-- > 0) {
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> ans;
+        priority_queue<vector<int>,vector<vector<int>>,comp> heap(points.begin(),points.end());
+        while(k-->0){
             ans.push_back(heap.top());
             heap.pop();
         }
