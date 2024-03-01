2a05eb47318fc242f91fc65de7e87c7e7b965695
Shivam <shivam97rules@gmail.com>
2024-03-01 18:04:31 -0500

    Time: 25 ms (61.12%), Space: 22.9 MB (23.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,16 +4,27 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
         ans.push_back(intervals.front());
-        int n=intervals.size();
-        for(int i=1;i<n;i++){
-            auto& curIntr=intervals[i];
-            auto& prevIntr=ans.back();
-            if(prevIntr[1]<curIntr[0]){
-                ans.push_back(curIntr);
+        for(auto& pair:intervals){
+            int prev_x=ans.back()[0];
+            int prev_y=ans.back()[1];
+            int curr_x=pair[0];
+            int curr_y=pair[1];
+            if(curr_x>prev_y){
+                ans.push_back(pair);
             }else{
-                ans[ans.size()-1][1]=max(ans[ans.size()-1][1],curIntr[1]);
+                if(curr_y<=prev_y){
+                    //skip
+                    continue;
+                }else{
+                    vector<int> new_pair(2,0);
+                    new_pair[0]=prev_x;
+                    new_pair[1]=curr_y;
+                    ans.erase(ans.end());
+                    ans.push_back(new_pair);
+                }
             }
         }
+        
         return ans;
     }
 };
