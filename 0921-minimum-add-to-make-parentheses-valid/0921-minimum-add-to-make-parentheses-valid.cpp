9461ec73632e0f472b3a84e2467a10c832cd4719
Shivam <shivam97rules@gmail.com>
2024-03-09 23:13:29 -0500

    Time: 3 ms (43.13%), Space: 7.5 MB (59.68%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -5,14 +5,13 @@ public:
         for(auto& ch:s){
             if(ch=='('){
                 st.push(ch);
+            }else if(ch==')' and !st.empty() and st.top()=='('){
+                st.pop();
             }else{
-                if(!st.empty() and st.top()=='('){
-                    st.pop();
-                }else{
-                    st.push(ch);
-                }
+                st.push(ch);
             }
         }
+        
         return st.size();
     }
 };
