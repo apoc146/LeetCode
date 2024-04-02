e4b0f9ec6e4e0e5046756a13def20ad607265dc4
Shivam <shivam97rules@gmail.com>
2024-04-02 19:28:38 -0400

    Time: 16 ms (27.36%), Space: 17.3 MB (36.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,34 +1,21 @@
-class comp{
-public:
-    bool operator()(pair<int,int> p1, pair<int,int> p2){
-        if(p2.second > p1.second){
-            return true;
-        }
-        return false;
-    }
-};
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int> ans;
         unordered_map<int,int> map;
-        for(auto& ele:nums){
-            map[ele]++;
-        }
-
-        priority_queue<pair<int,int>, vector<pair<int,int>>,comp> pq;
-        for(auto& it:map){
-            pq.push(make_pair(it.first,it.second));
+        for(auto ele:nums){
+            map[ele]+=1;
         }
         
-        while(k>0){
-            ans.push_back(pq.top().first);
-            pq.pop();
-            --k;
+        priority_queue<pair<int,int>> heap;
+        for(auto ele:map){
+            heap.push(make_pair(ele.second,ele.first));
         }
         
+        vector<int> ans;
+        while(k-->0){
+            ans.push_back(heap.top().second);
+            heap.pop();
+        }
         return ans;
-        
     }
 };
