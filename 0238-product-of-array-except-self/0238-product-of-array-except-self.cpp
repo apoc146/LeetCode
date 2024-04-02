8c3ac3afada0a5c93405dc14f8a00b3aa834dfeb
Shivam <shivam97rules@gmail.com>
2024-04-02 19:35:26 -0400

    Time: 28 ms (5.14%), Space: 40.1 MB (5.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    void printVec(vector<int> vec){
-        for(auto &ele:vec){
-            cout<<ele;
-        }
-        cout<<endl;
-    }
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int> preNum(n,0);
-        vector<int> postNum(n,0);
-        
-        preNum[0]=1;
-        for(int i=1;i<n;i++){
-            preNum[i]=preNum[i-1]*nums[i-1];
-        }
-        
-        printVec(preNum);
-        
-        postNum[n-1]=1;
-        for(int i=n-2;i>-1;i--){
-            postNum[i]=postNum[i+1]*nums[i+1];
-        }
-        
-        printVec(postNum);
-        
         vector<int> ans(n,0);
+        vector<int> pre(n,1);
+        vector<int> post(n,1);
+        
+        pre[0]=1;
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]*nums[i-1];
+        }
+        
+        post[n-1]=1;
+        for(int j=n-2;j>=0;j--){
+            post[j]=post[j+1]*nums[j+1];
+        }
+        
         for(int i=0;i<n;i++){
-            ans[i]=preNum[i]*postNum[i];
+            ans[i]=pre[i]*post[i];
         }
         
         return ans;
+        
     }
 };
