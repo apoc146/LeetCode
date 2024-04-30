dc197b320f08c78f41ccdbbcd239fd287798d4c5
Shivam <shivam97rules@gmail.com>
2024-04-30 01:46:20 -0400

    Time: 2994 ms (5.10%), Space: 722.9 MB (5.05%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
-    int dfs(int r, int c, vector<vector<int>> grid, vector<vector<int>>& visited){
-        int m=grid.size();
-        int n=grid[0].size();
+    bool check(int r, int c, vector<vector<int>> grid){
+        int n=grid.size();
+        int m=grid[0].size();
         
-        if(r<0 or r>=m or c<0 or c>=n or visited[r][c]==1 or grid[r][c]==0){
-            return 0;
+        if(r>=0 and r<n and c>=0 and c<m){
+            return true;
         }
         
+        return false;
+    }
+    
+    int dfs(int r, int c, vector<vector<int>>& visited, vector<vector<int>> grid){
+        if( check(r,c,grid)==false or visited[r][c]==1 or grid[r][c]==0){
+            return 0;
+        }
+
         visited[r][c]=1;
-        int n1=dfs(r,c-1,grid,visited);
-        int n2=dfs(r-1,c,grid,visited);
-        int n3=dfs(r,c+1,grid,visited);
-        int n4=dfs(r+1,c,grid,visited);
-        return 1+(n1+n2+n3+n4);
+        int ans=1;
+        ans+=dfs(r-1,c,visited, grid);
+        ans+=dfs(r,c-1,visited, grid);
+        ans+=dfs(r+1,c,visited, grid);
+        ans+=dfs(r,c+1,visited, grid);
+        
+        return ans;
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>> visited(m,vector<int>(n,0)); 
+        int n=grid.size();
+        int m=grid[0].size();
+        int mx=0;
         
-        int gMax=-1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                gMax=max(gMax,dfs(i,j,grid,visited));
+        vector<vector<int>> visited(n, vector<int>(m,0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]==0 and grid[i][j]==1){
+                    mx=max(mx,dfs(i,j,visited, grid));  
+                }
             }
         }
-        return gMax;
+        
+        return mx;
     }
 };
