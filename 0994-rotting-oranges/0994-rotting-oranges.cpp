2b2ad46338d487ada0a5367894f53661b1599feb
Shivam <shivam97rules@gmail.com>
2024-04-30 14:07:40 -0400

    Time: 4 ms (62.28%), Space: 16.5 MB (14.87%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,50 +1,55 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        using pii = pair<int, int>;
+        int n=grid.size();
+        int m=grid[0].size();
+        using pii= pair<int,int>;
         
-        queue<pii> q;
-        int fresh = 0;
+        vector<vector<int>> visited(n,vector<int>(m,0));
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
-                    q.push({i, j});
-                }else if(grid[i][j] == 1){
+        queue<pair<int,int>> q;
+        int fresh =0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
         
-        int time = 0;
-        vector<int> r_diff = {0, -1, 0, +1};
-        vector<int> c_diff = {-1, 0, +1, 0};
-        
-        while(!q.empty()){
-            int sz = q.size();
-            for(int i = 0; i < sz; i++){
-                pii node = q.front();
-                q.pop();
-                int x = node.first;
-                int y = node.second;
+        int time=0;
+        while(q.size()>0 and fresh>0){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
                 
-                // BFS
-                for(int idx = 0; idx < 4; idx++){
-                    int r_idx = x + r_diff[idx];
-                    int c_idx = y + c_diff[idx];
+                pii node=q.front();
+                q.pop();
+                int x=node.first;
+                int y=node.second;
+                
+                visited[x][y]=1;
+                vector<int> r_diff={0,-1,0,+1};
+                vector<int> c_diff={-1,0,+1,0};
+                
+                //bfs
+                for(int idx=0;idx<4;idx++){
+                    int r_idx=x+r_diff[idx];
+                    int c_idx=y+c_diff[idx];
                     
-                    if(r_idx >= 0 && r_idx < n && c_idx >= 0 && c_idx < m && grid[r_idx][c_idx] == 1){
-                        q.push({r_idx, c_idx});
-                        grid[r_idx][c_idx] = 2; // Mark as rotten
+                    if(r_idx >=0 and r_idx<n and c_idx>=0 and c_idx<m and visited[r_idx][c_idx]==0 and grid[r_idx][c_idx]==1){
+                        q.push({r_idx,c_idx});
+                        visited[r_idx][c_idx]=1;
+                        grid[r_idx][c_idx]=2;
                         fresh--;
                     }
                 }
+                
             }
-            if(!q.empty()) time++; // Increment time if there are still rotten oranges
+            time++;
         }
-        
-        return fresh == 0 ? time : -1;
+        return fresh == 0 ? time : -1; // Return -1 if there are still fresh oranges
     }
 };
